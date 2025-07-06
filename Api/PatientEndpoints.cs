2d41be5434e4d368b94e2d75124abee4239640e5
Morten Astrup <morten.astrup@madaster.com>
2025-07-06 14:05:54 +0200

    Refactor endpoints

--- Api/PatientEndpoints.cs
+++ Api/PatientEndpoints.cs
@@ -14,34 +14,47 @@ public static class PatientEndpoints
     public static void MapPatientEndpoints(this IEndpointRouteBuilder app)
     {
         var group = app.MapGroup("api/patients");
-        group.MapPost("", async ([FromBody] CreatePatientCommand command, ICommandHandler<CreatePatientCommand, Patient> handler, CancellationToken ct) =>
-        {
-            var patient = await handler.Handle(command, ct);
-            return Results.Ok(patient);
-        });
-        group.MapDelete("{id}", async (
-            [FromRoute] Guid id,
-            [FromServices] ICommandHandler<DeletePatientCommand, bool> handler,
-            CancellationToken ct) =>
-        {
-            var command = new DeletePatientCommand(id);
-            var patients = await handler.Handle(command, ct);
-            return Results.Ok(patients);
-        });
-        group.MapGet("", async (IQueryHandler<GetAllPatientsQuery, IEnumerable<Patient>> handler, CancellationToken ct) =>
-        {
-            var patients = await handler.Handle(new GetAllPatientsQuery(), ct);
-            return Results.Ok(patients);
-        });
-        group.MapGet("{id}", async (
-            [FromRoute] Guid id, 
-            [FromServices] IQueryHandler<GetPatientByIdQuery, Patient> handler, 
-            CancellationToken ct) =>
-        {
-            var query = new GetPatientByIdQuery(id);
-            var patients = await handler.Handle(query, ct);
-            return Results.Ok(patients);
-        });
+        group.MapPost("", CreatePatient).WithName(nameof(CreatePatient));
+        group.MapGet("", GetAllPatients).WithName(nameof(GetAllPatients));
+        group.MapGet("{id}", GetPatientById).WithName(nameof(GetPatientById));
+        group.MapDelete("{id}", DeletePatient).WithName(nameof(DeletePatient));
+    }
+
+    public static async Task<IResult> CreatePatient(
+        [FromBody] CreatePatientCommand command,
+        [FromServices] ICommandHandler<CreatePatientCommand, Patient> handler,
+        CancellationToken ct)
+    {
+        var patient = await handler.Handle(command, ct);
+        return Results.Ok(patient);
+    }
+
+    public static async Task<IResult> DeletePatient(
+        [FromRoute] Guid id,
+        [FromServices] ICommandHandler<DeletePatientCommand, bool> handler,
+        CancellationToken ct)
+    {
+        var command = new DeletePatientCommand(id);
+        var result = await handler.Handle(command, ct);
+        return Results.Ok(result);
+    }
+
+    public static async Task<IResult> GetAllPatients(
+        [FromServices] IQueryHandler<GetAllPatientsQuery, IEnumerable<Patient>> handler,
+        CancellationToken ct)
+    {
+        var patients = await handler.Handle(new GetAllPatientsQuery(), ct);
+        return Results.Ok(patients);
+    }
+
+    public static async Task<IResult> GetPatientById(
+        [FromRoute] Guid id,
+        [FromServices] IQueryHandler<GetPatientByIdQuery, Patient> handler,
+        CancellationToken ct)
+    {
+        var query = new GetPatientByIdQuery(id);
+        var patient = await handler.Handle(query, ct);
+        return Results.Ok(patient);
     }
 }
 
